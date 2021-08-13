183ba0b0f3c7835bd12c81fd903d0da8075d4311
Antonio Monroy <tony_monroe1212@hotmail.com>
2021-08-12 19:41:28 -0500

    Primer commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main(){ 
+    cout << "Bienvenido al curso, aqui encontraras todos los codigos elaborados por mi, en este curso" << endl;
+
+    return 0;
+}
